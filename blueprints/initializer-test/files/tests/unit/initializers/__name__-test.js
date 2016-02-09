b7c8835720ac5c82631678254bc8cf87706bb55b
samcic <sam@skedda.com>
2016-02-09 16:15:13 +0100

    Update __name__-test.js

--- blueprints/initializer-test/files/tests/unit/initializers/__name__-test.js
+++ blueprints/initializer-test/files/tests/unit/initializers/__name__-test.js
@@ -18,5 +18,5 @@ test('it works', function(assert) {
   <%= classifiedModuleName %>Initializer.initialize(application);
 
   // you would normally confirm the results of the initializer here
-  assert.ok(true);
+  // assert.ok(true);
 });
